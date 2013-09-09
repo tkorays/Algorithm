d546bcc94bd9260230903275d98b7d783b510dbf
jx <qing_cang@126.com>
2013-09-09 21:13:59 +0800

    add stack class using namespace XStack

---
+++ DS_Stack/LinkedStack.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * --------------------------------------
+ * @author tkorays <tkorays@hotmail.com>
+ * 
+ * @CreateTime 2013/9/8
+ * 
+ * @Description Linked Stack
+ * --------------------------------------
+ */
+using System;
+
+using LinearList;
+
+namespace XStack
+{
+	// 使用了以前写的SingleLinkedList类
+	public class LinkedStack<T>:SingleLinkedList<T>
+	{
+		private SingleLinkedNode<T> top;
+		public LinkedStack():base()
+		{
+			top = base.Head.Next;
+		}
+		public override bool Empty
+		{
+			get{
+				return top==null;
+			}
+		} 
+		public void Push(T k){
+			SingleLinkedNode<T> q = new SingleLinkedNode<T>(k);
+			q.Next = top;
+			top = q;
+			base.Head.Next=top;
+		}
+		public T Pop(){
+			T k = default(T);
+			if (!Empty) {
+				k=top.Item;
+				top= top.Next;
+				base.Head.Next = top;
+				return k;
+			}else{
+				throw new InvalidOperationException("Stack is Empty: "+this.GetType());
+			}
+		}
+		public T Peek(){
+			if (!Empty) {
+				return top.Item;
+			}else{
+				throw new InvalidOperationException("Stack is Empty: "+this.GetType());
+			}
+		}
+	}
+}
---
+++ DS_Stack/SequencedStack.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * --------------------------------------
+ * @author tkorays <tkorays@hotmail.com>
+ * 
+ * @CreateTime 2013/9/8
+ * 
+ * @Description Sequenced Stack
+ * --------------------------------------
+ */
+using System;
+
+namespace XStack
+{
+
+	public class SequencedStack<T>
+	{
+		private T[] items;
+		private const int empty = -1;
+		private int top = empty;
+		
+		public SequencedStack(int n)
+		{
+			items = new T[n];
+			top = empty;
+		}
+		public SequencedStack():this(16){}
+		
+		public int Count{
+			get{
+				return top+1;
+			}
+		}
+		public bool Empty{
+			get{
+				return top==empty;
+			}
+		}
+		public bool Full{
+			get{
+				return top>=items.Length-1;
+			}
+		}
+		public void Push(T k){
+			if (Full) {
+				DoubleCapacity();
+			}
+			top++;
+			items[top]=k;
+		}
+		private void DoubleCapacity(){
+			int count = Count;
+			int capacity = 2*items.Length;
+			T[] copy = new T[capacity];
+			for (int i = 0; i < count; i++) {
+				copy[i] = items[i];
+			}
+			items = copy;
+		}
+		public T Pop(){
+			T k = default(T);
+			if (!Empty) {
+				k=items[top];
+				top --;
+				return k;
+			}else{
+				throw new InvalidOperationException("Stack is Empty: "+this.GetType());
+			}
+		}
+		public T Peek(){
+			if (!Empty) {
+				return items[top];
+			}else{
+				throw new InvalidOperationException("Stack is Empty: "+this.GetType());
+			}
+		}
+		public void Show(bool showTypeName){
+			if (showTypeName) {
+				Console.Write("Stack: ");
+			}
+			if (!Empty) {
+				for (int i = this.top; i >= 0; i--) {
+					Console.Write(items[i]+" ");
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
